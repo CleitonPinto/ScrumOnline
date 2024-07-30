45299f13ef4b28103d00eb9da76b3891906bdc24
CleitonPinto <cleitonlupi@gmail.com>
2024-07-30 11:57:16 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React from 'react';
+import Header from './components/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <p>Bem-vindo à aplicação Scrum Online!</p>
+      <p>BY NEGREIRO!</p>
+    </div>
+  );
+}
+
+export default App;
+
